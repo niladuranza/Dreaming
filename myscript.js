4a218c6823be6b210817db9693e75a0590356f67
Nila <77408478+niladuranza@users.noreply.github.com>
2023-04-24 22:25:31 -0400

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,7 @@
+{/* <script>
+window.addEventListener('scroll', function() {
+  var overlayImage = document.querySelector('.overlay-image');
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  overlayImage.style.bottom = -scrollPosition + 'px';
+});
+</script> */}
